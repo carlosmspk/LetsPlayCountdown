fc80b703162bc00cc682722781784d5aeac707df
carlosmspk <carlos_cp_santos@hotmail.com>
2021-10-31 00:43:51 +0100

    Added all possibilities, rather than first one found

--- letters.py
+++ letters.py
@@ -16,20 +16,27 @@ def validate_word(letters : list, word : str) -> int:
             return 0
     return len(word)
 
-def get_best_word(letters : str) -> str:
+def get_best_word(letters : str) -> list:
     letter_list = [letter.lower() for letter in letters]
 
-    best_word = ""
+    best_words = []
     biggest_word_size = 0
 
     for word in tqdm(dictionary, "Looking through the dictionary..."):
         this_word_size = validate_word(letter_list, word)
         if this_word_size > biggest_word_size:
-            best_word = word
+            best_words.clear()
             biggest_word_size = this_word_size
+        if this_word_size == biggest_word_size:
+            best_words.append(word.upper())
 
-    return best_word.upper()
+    return best_words
 
 def run_letters():
     letters = input("Write all the letters in the board (e.g. 'AFSICEBBA')\n>>> ")
-    print("\nThe best word found is '" + get_best_word (letters) +"'\n")
+    print("\nThe best words found were...")
+    for i, word in enumerate(get_best_word(letters),1):
+        print(i, word)
+
+if __name__ == "__main__":
+    run_letters()
